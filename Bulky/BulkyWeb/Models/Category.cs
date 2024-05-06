32e1f6678bee2e9799eb1f30a9b8c7bf27f69cce
Noel <horizon33noela@gmail.com>
2024-05-06 06:09:26 -0600

    update

--- Bulky/BulkyWeb/Models/Category.cs
+++ Bulky/BulkyWeb/Models/Category.cs
@@ -10,8 +10,10 @@ namespace BulkyWeb.Models
 
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order must be between 1-100")]
         public int DisplayOrder { get; set; } 
     }
 }
--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a693c82a0f9d629fc7bbaafef44513aa3163ee7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7689c02a38b6b14867312cb91f38ec765630b855")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
